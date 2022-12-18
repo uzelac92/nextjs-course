0f7b34c0a6bd04bd64970bf651641e16b3d660c6
Vladimir Uzelac <uzelac92@yahoo.com>
2022-12-18 22:24:58 +0100

    Wesbite layout

---
+++ components/Layout.js
@@ -0,0 +1,39 @@
+import Head from 'next/head';
+import Link from 'next/link';
+import React from 'react';
+
+export default function Layout({ title, children }) {
+  return (
+    <>
+      <Head>
+        <title>{title ? title + ' - Amazona' : 'Amazona'}</title>
+        <meta name="description" content="Ecomerce website" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <div className="flex min-h-screen flex-col justify-between">
+        <header>
+          <nav className="flex h-12 justify-between shadow-md items-center px-4">
+            <Link className="text-lg font-bold" href="/">
+              amazona
+            </Link>
+            <div>
+              <Link href="/cart" className="px-2">
+                Cart
+              </Link>
+              <Link href="/login" className="p-2">
+                Login
+              </Link>
+            </div>
+          </nav>
+        </header>
+
+        <main className="container m-auto mt-4 px-4">{children}</main>
+
+        <footer className="flex justify-center items-center h-10 shadow-inner">
+          <p>Copyright © 2022 Amazona</p>
+        </footer>
+      </div>
+    </>
+  );
+}
