a67f2fe714930d5986569d552ddbe2592ce22baa
Vladimir Uzelac <uzelac92@yahoo.com>
2022-12-18 23:00:29 +0100

    product items

---
+++ components/ProductItem.js
@@ -0,0 +1,30 @@
+/* eslint-disable @next/next/no-img-element */
+import Link from 'next/link';
+import React from 'react';
+
+export default function ProductItem({ product }) {
+  return (
+    <div className="cart">
+      <Link href={`/product/${product.slug}`} legacyBehavior>
+        <a>
+          <img
+            src={product.image}
+            alt={product.name}
+            className="rounded shadow"
+          />
+        </a>
+      </Link>
+
+      <div className="flex flex-col items-center justify-center p-5">
+        <Link href={`/product/${product.slug}`} className="h2 text-lg">
+          {product.name}
+        </Link>
+        <p className="mb-2">{product.brand}</p>
+        <p>${product.price}</p>
+        <button className="primary-button" type="button">
+          Add to cart
+        </button>
+      </div>
+    </div>
+  );
+}
